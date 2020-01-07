9008b96718d92201f498c6afec6bc6f10e8ddf26
Alexej <alexej4703@gmail.com>
2020-01-07 23:54:36 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,258 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import redirect
+from flask import session
+from flask import url_for
+
+import psycopg2
+from psycopg2.extras import DictCursor
+
+from dbconfig import dbconfig
+from user import Student
+from forms import *
+
+app = Flask(__name__)
+app.secret_key = 'key'
+
+# **********************************************************
+# Главная страница
+@app.route('/')
+@app.route('/index')
+def index():
+	if not session.get('acc_type'):
+		return redirect(url_for('login'))
+
+	return render_template('index.html')
+
+# **********************************************************
+# Вход в систему
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+	form = LoginForm(request.form)
+	if request.method == 'POST' and form.validate():
+			login = request.form.get('login', '')
+			password = request.form.get('password', '')
+
+			if login != 'admin' and password != 'admin':
+				conn = psycopg2.connect(dbname = dbconfig['dbname'],
+										user = dbconfig['user'],
+										password = dbconfig['password'],
+										host = dbconfig['host'])
+				cursor = conn.cursor()
+				sql = '''select * from "user" where "login" = '{}' and password = '{}' '''.format(login, password)
+				cursor.execute(sql)
+				rec = cursor.fetchone()
+
+				if rec:
+					session['id'] = rec[0]
+					session['login'] = rec[1]
+					session['name'] = rec[3]
+					session['surname'] = rec[4]
+					session['patronymic'] = rec[5]
+					session['acc_type'] = rec[6];
+
+				cursor.close()
+				conn.close()
+			else:
+				session['acc_type'] = 3;
+
+	if session.get('acc_type'):
+		return redirect(url_for('index'))
+	else:
+		return render_template('login.html', form = form)
+
+# **********************************************************
+# Регистрация в системе
+@app.route('/registration', methods = ['POST', 'GET'])
+def registration():
+	if request.args.get('acc_type', '') == 'student':
+		acc_type = 1
+		form = RegistrationStudentForm(request.form)
+	elif request.args.get('acc_type', '') == 'teacher':
+		acc_type = 2
+		form = RegistrationTeacherForm(request.form)
+
+	if request.method == 'POST' and form.validate():
+		print('kkk')
+		login = request.form.get('login', '')
+		password = request.form.get('password', '')
+		name = request.form.get('name', '')
+		surname = request.form.get('surname', '')
+		patronymic = request.form.get('patronymic', '')
+
+		# Проверяем правильность пароля
+		if password != request.form.get('confirm_password', ''):
+			print('Check password')
+			return render_template('registration.html', form = form, acc_type = acc_type)
+
+		# Записываем в базу данные пользователя
+		conn = psycopg2.connect(dbname = dbconfig['dbname'],
+								user = dbconfig['user'],
+								password = dbconfig['password'],
+								host = dbconfig['host'])
+		cursor = conn.cursor()
+		sql = '''insert into "user"("login", "password", "name", "surname", "patronymic", "user_type")'''
+		sql += '''values('{}', '{}', '{}', '{}', '{}', '{}') returning "id"'''.format(login, password, name, surname, patronymic, acc_type)
+		cursor.execute(sql)
+		id = cursor.fetchone()[0];
+
+
+		if acc_type == 1:
+			# Регистрация студента
+			group_number = request.form.get('group_number', '')
+
+			# Запишем в базу данные о студенте
+			sql = '''insert into "student"("user_id", "group_number") values('{}', '{}')'''.format(id, group_number)
+			cursor.execute(sql)
+
+		elif acc_type == 2:
+			print('teach')
+			# Регистрация преподавателя
+			science_degree = request.form.get('science_degree', '')
+			number_of_publications = request.form.get('number_of_publications', '')
+			department_number = request.form.get('department_number', '')
+
+			# Запишем в базу данные о преподавателе
+			sql = '''insert into "teacher"("user_id", "science_degree", "number_of_publications", "number_of_department")'''
+			sql += '''values('{}', '{}', '{}', '{}')'''.format(id, science_degree, number_of_publications, department_number)
+			cursor.execute(sql)
+
+		# Закрытие соединения с базой
+		conn.commit()
+		cursor.close()
+		conn.close()
+
+		return redirect(url_for('index'))
+
+	return render_template('registration.html', form = form, acc_type = acc_type)
+
+# **********************************************************
+# Выход из профиля
+@app.route('/logout')
+def logout():
+	# Удаление сессий
+	session.pop('id', None)
+	session.pop('login', None)
+	session.pop('name', None)
+	session.pop('surname', None)
+	session.pop('patronymic', None)
+	session.pop('acc_type', None)
+	return redirect(url_for('login'))
+
+# **********************************************************
+# Панель администратора
+@app.route('/control_panel', methods = ['GET', 'POST'])
+def control_panel():
+	rec, form = update_control_panel_content();
+	if request.method == 'POST' and form.validate():
+		if request.args.get('act', '') == 'department':
+			department_number = request.form.get('department_number', '')
+			sql = '''insert into "department"("number") values('{}')'''.format(department_number)
+		elif request.args.get('act', '') == 'speciality':
+			speciality_number = request.form.get('speciality_number', '')
+			speciality_name = request.form.get('speciality_name', '')
+			sql = '''insert into "speciality"("number", "name") values('{}', '{}')'''.format(speciality_number, speciality_name)
+		elif request.args.get('act', '') == 'semester_curriculum':
+			id = request.form.get('id', '')
+			semester_number = request.form.get('semester_number', '')
+			speciality_number = request.form.get('speciality_number', '')
+			sql = '''insert into "semester_curriculum"("semester_number", "speciality_number") values('{}', '{}')'''.format(semester_number, speciality_number)
+		elif request.args.get('act', '') == 'subject_in_the_curriculum':
+			subject_name = request.form.get('subject_name', '')
+			number_of_lection = request.form.get('number_of_lection', '')
+			number_of_labs = request.form.get('number_of_labs', '')
+			kr = request.form.get('kr', '')
+			if kr == '': kr = 'n'
+			semester_id = request.form.get('semester_id', '')
+			sql = '''insert into "subject_in_the_curriculum"("name", "number_of_lection", "number_of_labs", "kr", "semester_curriculum_id") values('{}', '{}', '{}', '{}', '{}')'''.format(subject_name, number_of_lection, number_of_labs, kr, semester_id)
+		elif request.args.get('act', '') == 'lesson':
+			day_of_the_week = request.form.get('day_of_the_week', '')
+			serial_number = request.form.get('serial_number', '')
+			subject_type = request.form.get('subject_type', '')
+			audience_number = request.form.get('audience_number', '')
+			subject_id = request.form.get('subject_id', '')
+			sql = '''insert into "lesson"("day_of_the_week", "serial_number", "subject_type", "audience_number", "subject_id") values('{}', '{}', '{}', '{}', '{}')'''.format(day_of_the_week, serial_number, subject_type, audience_number, subject_id)
+
+		# Открытие соединения с БД
+		conn = psycopg2.connect(dbname = dbconfig['dbname'],
+								user = dbconfig['user'],
+								password = dbconfig['password'],
+								host = dbconfig['host'])
+		cursor = conn.cursor()
+		# Выполнение записи в БД
+		cursor.execute(sql)
+
+		# Закрытие соединения с БД
+		conn.commit()
+		cursor.close()
+		conn.close()
+
+		rec, form = update_control_panel_content();
+
+	# Удаление строк из БД
+	if request.args.get('del', ''):
+		conn = psycopg2.connect(dbname = dbconfig['dbname'],
+								user = dbconfig['user'],
+								password = dbconfig['password'],
+								host = dbconfig['host'])
+		cursor = conn.cursor()
+		if request.args.get('act', '') == 'department':
+			sql = '''delete from "department" where "number" = {}'''.format(request.args.get('del', ''))
+		elif request.args.get('act', '') == 'speciality':
+			sql = '''delete from "speciality" where "number" = {}'''.format(request.args.get('del', ''))
+		elif request.args.get('act', '') == 'semester_curriculum':
+			sql = '''delete from "semester_curriculum" where "id" = {}'''.format(request.args.get('del', ''))
+
+		cursor.execute(sql)
+		# Закрытие соединения с БД
+		conn.commit()
+		cursor.close()
+		conn.close()
+		return redirect(url_for('control_panel', act = request.args.get('act')))
+
+	return render_template('control_panel.html', form = form, rec = rec)
+
+# **********************************************************
+def update_control_panel_content():
+	conn = psycopg2.connect(dbname = dbconfig['dbname'],
+							user = dbconfig['user'],
+							password = dbconfig['password'],
+							host = dbconfig['host'])
+	cursor = conn.cursor()
+
+	if request.args.get('act', '') == 'department':
+		form = AddDepartment(request.form)
+		sql = '''select * from "department"'''
+	elif request.args.get('act', '') == 'speciality':
+		form = AddSpeciality(request.form)
+		sql = '''select * from "speciality"'''
+	elif request.args.get('act', '') == 'semester_curriculum':
+		form = AddSemesterCurriculum(request.form)
+		sql = '''select * from "semester_curriculum"'''
+	elif request.args.get('act', '') == 'subject_in_the_curriculum':
+		form = AddSubjectInTheCurriculum(request.form)
+		sql = '''select * from "subject_in_the_curriculum"'''
+	elif request.args.get('act', '') == 'lesson':
+		form = AddLesson(request.form)
+		sql = '''select * from "lesson"'''
+	elif request.args.get('act', '') == 'group':
+		form = AddLesson(request.form)
+		sql = '''select * from "group"'''
+	elif request.args.get('act', '') == 'audience':
+		form = AddAudience(request.form)
+		sql = '''select * from "audience"'''
+
+	cursor.execute(sql)
+	return cursor.fetchall(), form
+
+# **********************************************************
+# Редактирование личного профиля
+@app.route('/profile')
+def profile():
+	if session['acc_type'] == 1:
+		form = profileStudentForm(request.form)
+	elif session['acc_type'] == 2:
+		form = profileTeacherForm(request.form)
+
+	return render_template('profile.html', form = form)
---
+++ forms.py
@@ -0,0 +1,181 @@
+import psycopg2
+from psycopg2.extras import DictCursor
+from wtforms import Form, StringField, PasswordField, SubmitField, SelectField, IntegerField, BooleanField
+from wtforms.validators import Required, NumberRange
+
+from dbconfig import dbconfig
+
+# ********************************
+class LoginForm(Form):
+    login = StringField('login', validators = [Required()])
+    password = PasswordField('password', validators = [Required()])
+    submit = SubmitField('Вход')
+
+# ********************************
+class RegistrationStudentForm(Form):
+    login = StringField('login', validators = [Required()])
+    password = PasswordField('password', validators = [Required()])
+    confirm_password = PasswordField('confirm_password', validators = [Required()])
+    surname = StringField('surname', validators = [Required()])
+    name = StringField('name', validators = [Required()])
+    patronymic = StringField('patronymic', validators = [Required()])
+    submit = SubmitField('Отправить')
+    conn = psycopg2.connect(dbname = dbconfig['dbname'],
+                            user = dbconfig['user'],
+                            password = dbconfig['password'],
+                            host = dbconfig['host'])
+    cursor = conn.cursor()
+    sql = '''select "number" from "group"'''
+    cursor.execute(sql)
+    rec = cursor.fetchall()
+
+    if rec:
+        choices = []
+        if len(rec) > 0:
+            for i in rec:
+                choices += [(str(i[0]), str(i[0]))]
+    else:
+        choices = [(0, None)]
+    cursor.close()
+    conn.close()
+
+    group_number = SelectField('group_number', choices = choices)
+
+# ********************************
+class RegistrationTeacherForm(Form):
+    login = StringField('login', validators = [Required()])
+    password = PasswordField('password', validators = [Required()])
+    confirm_password = PasswordField('confirm_password', validators = [Required()])
+    surname = StringField('surname', validators = [Required()])
+    name = StringField('name', validators = [Required()])
+    patronymic = StringField('patronymic', validators = [Required()])
+    science_degree = StringField('science_degree', validators = [Required()])
+    number_of_publications = IntegerField('number_of_publications', validators = [NumberRange(0, 500)])
+    submit = SubmitField('Отправить')
+
+    conn = psycopg2.connect(dbname = dbconfig['dbname'],
+                            user = dbconfig['user'],
+                            password = dbconfig['password'],
+                            host = dbconfig['host'])
+    cursor = conn.cursor()
+    sql = '''select "number" from "department"'''
+    cursor.execute(sql)
+    rec = cursor.fetchall()
+
+    if rec:
+        choices = []
+        if len(rec) > 0:
+            for i in rec:
+                choices += [(str(i[0]), str(i[0]))]
+    else:
+        choices = [(0, None)]
+    cursor.close()
+    conn.close()
+
+    department_number = SelectField('department_number', choices = choices)
+
+# ********************************
+class profileStudentForm(Form):
+    login = StringField('login', validators = [Required()])
+    surname = StringField('surname', validators = [Required()])
+    name = StringField('name', validators = [Required()])
+    patronymic = StringField('patronymic', validators = [Required()])
+    submit = SubmitField('Сохранить')
+    conn = psycopg2.connect(dbname = dbconfig['dbname'],
+                            user = dbconfig['user'],
+                            password = dbconfig['password'],
+                            host = dbconfig['host'])
+    cursor = conn.cursor()
+    sql = '''select "number" from "group"'''
+    cursor.execute(sql)
+    rec = cursor.fetchall()
+
+    if rec:
+        choices = []
+        if len(rec) > 0:
+            for i in rec:
+                choices += [(str(i[0]), str(i[0]))]
+    else:
+        choices = [(0, None)]
+    cursor.close()
+    conn.close()
+
+    group_number = SelectField('group_number', choices = choices)
+
+# ********************************
+class profileTeacherForm(Form):
+    login = StringField('login', validators = [Required()])
+    surname = StringField('surname', validators = [Required()])
+    name = StringField('name', validators = [Required()])
+    patronymic = StringField('patronymic', validators = [Required()])
+    submit = SubmitField('Сохранить')
+    science_degree = StringField('science_degree', validators = [Required()])
+    number_of_publications = IntegerField('number_of_publications', validators = [NumberRange(0, 500)])
+
+    conn = psycopg2.connect(dbname = dbconfig['dbname'],
+                            user = dbconfig['user'],
+                            password = dbconfig['password'],
+                            host = dbconfig['host'])
+    cursor = conn.cursor()
+    sql = '''select "number" from "department"'''
+    cursor.execute(sql)
+    rec = cursor.fetchall()
+
+    if rec:
+        choices = []
+        if len(rec) > 0:
+            for i in rec:
+                choices += [(str(i[0]), str(i[0]))]
+    else:
+        choices = [(0, None)]
+    cursor.close()
+    conn.close()
+
+    department_number = SelectField('department_number', choices = choices)
+
+# ********************************
+class AddDepartment(Form):
+    department_number = IntegerField('department_number', validators = [NumberRange(0, 50)])
+    submit = SubmitField('Добавить')
+
+# ********************************
+class AddSpeciality(Form):
+    speciality_number = IntegerField('speciality_number', validators = [NumberRange(0, 50)])
+    speciality_name = StringField('speciality_name', validators = [Required()])
+    submit = SubmitField('Добавить')
+
+# ********************************
+class AddSemesterCurriculum(Form):
+    semester_number = IntegerField('semester_number', validators = [NumberRange(0, 12)])
+    speciality_number = IntegerField('speciality_number', validators = [NumberRange(0, 50)])
+    submit = SubmitField('Добавить')
+
+# ********************************
+class AddSubjectInTheCurriculum(Form):
+    subject_name = StringField('subject_name', validators = [Required()])
+    number_of_lection = IntegerField('number_of_lection', validators = [NumberRange(0, 40)])
+    number_of_labs = IntegerField('number_of_lection', validators = [NumberRange(0, 40)])
+    kr = BooleanField('kr')
+    semester_id = IntegerField('semester_id')
+    submit = SubmitField('Добавить')
+
+# ********************************
+class AddLesson(Form):
+    day_of_the_week = IntegerField('day_of_the_week', validators = [NumberRange(1, 7)])
+    serial_number = IntegerField('serial_number', validators = [NumberRange(1, 10)])
+    subject_type = IntegerField('subject_type', validators = [NumberRange(1, 2)])
+    audience_number = IntegerField('subject_type', validators = [NumberRange(1, 999)])
+    subject_id = IntegerField('subject_id')
+    submit = SubmitField('Добавить')
+
+# ********************************
+class AddGroup(Form):
+    group_number = IntegerField('group_number', validators = [NumberRange(0, 9999)])
+    current_sem = IntegerField('current_sem', validators = [NumberRange(0, 12)])
+    speciality_number = IntegerField('speciality_number', validators = [NumberRange(0, 50)])
+    submit = SubmitField('Добавить')
+
+# ********************************
+class AddAudience(Form):
+    audience_number = IntegerField('audience_number', validators = [NumberRange(0, 999)])
+    submit = SubmitField('Добавить')
